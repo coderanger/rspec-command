95988088cb6e73a2083cfcb00ee6e8ab38701b88
Noah Kantrowitz <noah@coderanger.net>
2015-03-11 13:31:41 -0700

    Moar tests for Rake helper.

--- spec/rake_spec.rb
+++ spec/rake_spec.rb
@@ -32,7 +32,9 @@ task 'mytask' do
 end
 EOH
       rake_task 'mytask'
-      its(:stdout) { is_expected.to include "complete\n" }
+      its(:stdout) { is_expected.to eq "complete\n" }
+      its(:stderr) { is_expected.to eq '' }
+      its(:exitstatus) { is_expected.to eq 0 }
     end # /context with a simple task
 
     context 'with an environment variable' do
@@ -43,9 +45,65 @@ end
 EOH
       environment MYVAR: 'envvar'
       rake_task 'mytask'
-      its(:stdout) { is_expected.to include "envvar\n" }
+      its(:stdout) { is_expected.to eq "envvar\n" }
+      its(:stderr) { is_expected.to eq '' }
+      its(:exitstatus) { is_expected.to eq 0 }
       it { expect(ENV['MYVAR']).to be_nil }
     end # /context with an environment variable
+
+    context 'with no rakefile' do
+      rake_task 'mytask'
+      its(:stderr) { is_expected.to include 'No Rakefile found' }
+      its(:exitstatus) { is_expected.to eq 1 }
+    end # /context with no rakefile
+
+    context 'with a non-existent task' do
+      rakefile ''
+      rake_task 'mytask'
+      its(:stderr) { is_expected.to include "Don't know how to build task 'mytask'" }
+      its(:exitstatus) { is_expected.to eq 1 }
+    end # /context with a non-existent task
+
+    context 'with a task with arguments' do
+      rakefile <<-'EOH'
+task 'mytask', %w{arg1 arg2} do |t, args|
+  args.with_defaults(arg2: 'default')
+  puts "#{args[:arg1]} #{args[:arg2]}"
+end
+EOH
+      rake_task 'mytask', 'one'
+      its(:stdout) { is_expected.to eq "one default\n" }
+      its(:stderr) { is_expected.to eq '' }
+      its(:exitstatus) { is_expected.to eq 0 }
+    end # /context with a task with arguments
+
+    context 'with a task that fails' do
+      rakefile <<-EOH
+task 'failure' do
+  puts 'before'
+  raise "OMG"
+  puts 'after'
+end
+EOH
+      rake_task 'failure'
+      its(:stdout) { is_expected.to eq "before\n" }
+      its(:stderr) { is_expected.to include "Rakefile:3:in `block in <top (required)>': OMG (RuntimeError)" }
+      its(:exitstatus) { is_expected.to eq 1 }
+    end # /context with a task that fails
+
+    context 'with a task that fails with a specific exitstatus' do
+      rakefile <<-EOH
+task 'specific_failure' do
+  puts 'specific before'
+  Kernel.exit(42)
+  puts 'specific after'
+end
+EOH
+      rake_task 'specific_failure'
+      its(:stdout) { is_expected.to eq "specific before\n" }
+      its(:stderr) { is_expected.to eq '' }
+      its(:exitstatus) { is_expected.to eq 42 }
+    end # /context with a task that fails with a specific exitstatus
 
     context 'regression test for require-based Rakefiles and multiple tests' do
       file 'mytask.rb', 'task :mytask do puts "complete" end'
